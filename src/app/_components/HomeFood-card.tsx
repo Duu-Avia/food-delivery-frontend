86c8179d1cc0a2d5b1726e6b846bc4a376494a3a
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-01-27 18:25:49 +0800

    started food adding

--- src/app/_components/HomeFood-card.tsx
+++ src/app/_components/HomeFood-card.tsx
@@ -1,21 +1,14 @@
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Select } from "@/components/ui/select";
 import { DialogClose } from "@radix-ui/react-dialog";
-import { Pencil, Trash } from "lucide-react";
+import { Minus, Pencil, Plus, Trash } from "lucide-react";
 import { useEffect, useState } from "react";
 const CLOUDINARY_CLOUD_NAME = "dku0azubr";
 export const HomeFoodCard = ({ itemsId }) => {
   const [cardFoodData, setCardFoodData] = useState([]);
+  const [qty, setQty] = useState(0);
   useEffect(() => {
     const fetchFoods = async () => {
       const response = await fetch(`http://localhost:8000/dishes/${itemsId}`);
@@ -25,17 +18,11 @@ export const HomeFoodCard = ({ itemsId }) => {
     fetchFoods();
   }, []);
 
-
-
   return (
     <>
       <div className="flex gap-5 p-2">
-      
         {cardFoodData?.map((food?: any) => (
-          <div
-            className="relative p-3 border-solid border-[1px] border-[#E4E4E7] rounded-lg w-[270px] h-[241px]"
-            key={`foody-${food?._id}`}
-          >
+          <div className="relative p-3 border-solid border-[1px] border-[#E4E4E7] bg-[#FFFFFF] rounded-lg w-[270px] h-[241px]" key={`foody-${food?._id}`}>
             <div className="w-[238px] h-[129px]">{food?.image ? <img className="w-[238px] h-[129px] rounded-xl" src={food?.image || null} /> : <div className="w-[238px] h-[129px] bg-gray-100 flex justify-center items-center">No image</div>}</div>
             <div className="flex items-center justify-between">
               <div className="text-[1rem] text-[#EF4444] font-[500]">{food?.foodName}</div>
@@ -44,29 +31,59 @@ export const HomeFoodCard = ({ itemsId }) => {
             <div className="text-[#09090B] w-[100%]">{food?.ingredients}</div>
             <Dialog>
               <DialogTrigger asChild>
-                <Button
-                  className="absolute top-[70px] left-[200px] rounded-full size-[45px] bg-[#FFFFFF]"
-                  variant="outline"
-                >
-                  <Pencil className="text-[#EF4444] " />
+                <Button className=" absolute top-[70px] left-[200px] rounded-full size-[45px] bg-[#FFFFFF]" variant="outline">
+                  <Plus className="text-[#EF4444] " />
                 </Button>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[472px]">
+              <DialogContent className="max-w-[826px]">
                 <DialogHeader>
-                  <DialogTitle>Dishes info</DialogTitle>
-                  <DialogDescription></DialogDescription>
-                </DialogHeader>
+                  <DialogTitle className="">
+                    <div className="flex">
+                      <div>
+                        <img className="w-[377px] h-[364px] rounded-xl" src={food?.image} alt="" />
+                      </div>
+                      <div className="pl-[19px] text-[1rem] text-[#09090B] font-[400]">
+                        <div className="text-[1.7rem] text-[#EF4444] font-[500] pt-8">{food?.foodName}</div>
+                        <div className="pt-5">{food?.ingredients}</div>
+                        <div className="pt-[180px] gap-10">
+                          <div className="flex ">
+                            <div>
+                              <p>Total price</p>
+                              <div className="font-[600] pr-[200px]">${food?.price}</div>
+                            </div>
+                            <div>
+                              <div className="flex items-center gap-3">
+                                <Button
+                                  onClick={() => {
+                                    if (qty > 0) {
+                                      setQty(qty - 1);
+                                    }
+                                  }}
+                                  className="size-[44px] rounded-full"
+                                  variant={"outline"}>
+                                  <Minus />
+                                </Button>
+                                <div>{qty}</div>
+                                <Button onClick={() => setQty(qty + 1)} className="size-[44px] rounded-full" variant={"outline"}>
+                                  <Plus />
+                                </Button>
+                              </div>
+                            </div>
+                          </div>
 
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button className="bg-white  border-[1px] border-[#EF444480] mr-[250px]">
-                      <Trash className="text-[#EF4444]" />
-                    </Button>
-                  </DialogClose>
-                  <DialogClose asChild>
-                    <Button type="button">Save changes</Button>
-                  </DialogClose>
-                </DialogFooter>
+                          <div>
+                            <DialogClose asChild>
+                              <Button className="w-full" type="button">
+                                Add to card
+                              </Button>
+                            </DialogClose>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </DialogTitle>
+                </DialogHeader>
+                <DialogFooter></DialogFooter>
               </DialogContent>
             </Dialog>
           </div>
